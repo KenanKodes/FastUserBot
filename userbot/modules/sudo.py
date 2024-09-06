bdb7005a6ef612d20b397a3370eea049544ab299
KenanKodes <75477114+KenanKodes@users.noreply.github.com>
2024-09-06 21:17:57 +0400

    Update sudo.py

--- userbot/modules/sudo.py
+++ userbot/modules/sudo.py
@@ -5,10 +5,10 @@ from userbot import SUDO_ID
 from userbot.cmdhelp import CmdHelp
 from userbot.events import register
 
-@register(incoming=True, from_users=SUDO_ID, pattern="^.salive$")
+@register(incoming=True, from_users=SUDO_ID, pattern="^.falives$")
 async def _(q):
-    await q.client.send_message(q.chat_id,"`[⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝](@silgiuserbot)ＳＵＤＯ ✨`")
+    await q.client.send_message(q.chat_id,"`[⚝ 𝑭𝑨𝑺𝑻 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝](@TheFastSupp)ＳＵＤＯ ✨`")
 
 CmdHelp('sudo').add_command(
-    'salive', None, 'SUDOnun aktiv olub olmadığını yoxlayar.'
+    'falives', None, 'SUDOnun aktiv olub olmadığını yoxlayar.'
 ).add()
